5824d89cd23cdac057cf3c18aa6b72aac1aa7943
GaneshMumbai2001 <ganeshmumbai2001@gmail.com>
2024-07-01 11:03:13 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -7,6 +7,7 @@ def nada_main():
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
     # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    result = my_int1 + my_int2
 
-    return [Output(my_int1, "my_output", party1)]
+    # change the output below to be your new output
+    return [Output(result, "result", party1)]
